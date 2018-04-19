5883b991c61080ca9f44e8bd047abc8596a066ab
Lukáš Hejtmánek <hejtmy@gmail.com>
2018-04-19 14:36:55 -0700

    Adds vue and konva

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import axios from 'axios';
+import VueKonva from 'vue-konva';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import router from './router';
 import store from './store';
 
+Vue.use(VueKonva);
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
