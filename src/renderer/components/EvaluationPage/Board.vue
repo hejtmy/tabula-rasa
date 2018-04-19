e5bd3f1034f49d17a4e848d2f6df518db90fc94e
Lukáš Hejtmánek <hejtmy@gmail.com>
2018-04-19 14:42:00 -0700

    Adds basic conva thing

--- src/renderer/components/EvaluationPage/Board.vue
+++ src/renderer/components/EvaluationPage/Board.vue
@@ -1,8 +1,28 @@
 <template>
-  <p> THing being said</p>
+  <v-stage ref="stage" :config="config">
+    <v-layer ref="layer">
+      <v-circle :config="configCircle"></v-circle>
+    </v-layer>
+  </v-stage>
 </template>
 <script>
 export default {
   name: 'board',
+  data() {
+    return {
+      config: {
+        width: 500,
+        height: 500,
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 4,
+      },
+    };
+  },
 };
 </script>
